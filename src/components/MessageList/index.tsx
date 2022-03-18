bb2245a0443539df630a01c4a6d2b29e4e890efe
caiohenrique-developer <caiorussellrox@msn.com>
2022-03-18 15:30:43 -0300

    ✨ [feat]: Connecting may heat-back api with axios

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,57 +1,47 @@
 import styles from './style.module.scss';
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string;
+    }
+}
+
 export function MessageList() {
+    const [ messages, setMessages ] = useState<Message[]>([])
+    
+    useEffect(() => {
+        api.get<Message[]>('messages/last3').then(response => {
+            setMessages(response.data);
+        })
+    }, [])
+    
     return (
         <div className={styles.messageListWrapper}>
             <img src={logoImg} alt="DoWhile 2021" />
 
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/caiohenrique-developer.png" alt="Caio Henrique" />
-                        </div>
-                        <span>Caio Henrique</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/caiohenrique-developer.png" alt="Caio Henrique" />
-                        </div>
-                        <span>Caio Henrique</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/caiohenrique-developer.png" alt="Caio Henrique" />
-                        </div>
-                        <span>Caio Henrique</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/caiohenrique-developer.png" alt="Caio Henrique" />
-                        </div>
-                        <span>Caio Henrique</span>
-                    </div>
-                </li>
+                {
+                    messages.map(({ id, text, user: { name, avatar_url }}: Message) => (
+                        <li key={id} className={styles.message}>
+                            <p className={styles.messageContent}>{text}</p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImage}>
+                                    <img src={avatar_url} alt={name} />
+                                </div>
+                                <span>{name}</span>
+                            </div>
+                        </li>
+                        )
+                    )
+                }
+                
             </ul>
         </div>
     )
