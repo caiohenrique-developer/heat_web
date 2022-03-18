e215fd868ab7679350003ef2f6ad060adab308b3
caiohenrique-developer <caiorussellrox@msn.com>
2022-03-18 16:30:52 -0300

    ✨ [feat]: Create login feature

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -3,10 +3,12 @@ import { VscGithubInverted } from 'react-icons/vsc'
 import styles from './style.module.scss';
 
 export function LoginBox() {
+    const signInUrl = `https://github.com/login/oauth/authorize?client_id=5d2c74df6861a057ac24`;
+    
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
+            <a href={signInUrl} className={styles.signInWithGithub}>
                 <VscGithubInverted size="24" />
                 Entrar com GitHub
             </a>
